f6d6c4fb272f22777b3dbf5aa601d765cc06cf36
kr <hoylfree@gmail.com>
2022-04-13 14:09:10 +0900

    .env

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBnvwW6IXuDiM7xnM-1OlOtUIdRYph3ZsI",
-  authDomain: "my-own-dictionary-11804.firebaseapp.com",
-  projectId: "my-own-dictionary-11804",
-  storageBucket: "my-own-dictionary-11804.appspot.com",
-  messagingSenderId: "11421794204",
-  appId: "1:11421794204:web:f5c1625b1faefc7e945259",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 initializeApp(firebaseConfig);
